cae5de919aabccbac392b7a7e9f67fb0e96113ba
joopark <joopark@naver.com>
2019-02-12 07:39:38 +0900

    쿼리끝에 ; 추가

--- Queries/3장/SQLRcp05.sql
+++ Queries/3장/SQLRcp05.sql
@@ -5,14 +5,14 @@ select user_id
        when register_device = 2 then 'smartphone'
        when register_device = 3 then 'application'
   end as device_name
-from "053_mst_users"
+from "053_mst_users";
 
 --Page 56
 
 select stamp
   , substring(referrer  from 'https?://([^/]*)') AS referrer_host
  --Hive ,parse_url(referrer, 'HOST') AS referrer_host
-from "056_access_log"
+from "056_access_log";
 
 
 --Page 57
@@ -23,7 +23,7 @@ select stamp
   , substring(url  from 'id=([^&]*)') AS id
  --Hive ,parse_url(url, 'PATH') AS path
  --Hive ,parse_url(url, 'QUERT', 'id') AS id
-from "056_access_log"
+from "056_access_log";
 
 
 --Page 58
@@ -35,13 +35,13 @@ select
   , split_part(substring(url  from '//[^/]+([^?#]+)'),'/',3) AS path2
  --Hive ,split(parse_url(url, 'PATH','/') AS path1
  --Hive ,split(parse_url(url, 'QUERT', '/') AS path2
-from "056_access_log"
+from "056_access_log";
 
 
 --Page 58
 select
     current_date AS dt
-  , current_timestamp AS stamp
+  , current_timestamp AS stamp;
  --Hive ,  current_date() AS dt
  --Hive , current_timestamp() AS stamp
 
@@ -67,7 +67,7 @@ select
 --Hive , MONTH ( STAMP) AS Month
 --Hive , DAY ( STAMP) AS Day
 --Hive , Hours ( STAMP) AS Hour
-from (select CAST('2016-01-30 12:00:00' AS timestamp) as STAMP ) AS t1
+from (select CAST('2016-01-30 12:00:00' AS timestamp) as STAMP ) AS t1;
 
 
 --Page63
@@ -85,7 +85,7 @@ select
 --Hive , substr(STAMP,1,7) AS Year_month
 from (select CAST('2016-01-30 12:00:00' AS text) as STAMP ) AS t2
 --Hive from (select CAST('2016-01-30 12:00:00' AS string) as STAMP ) AS t2
-
+;
   --Page64
 Select
   purchase_id
@@ -93,4 +93,4 @@ Select
   , coupon
   , amount - coupon AS discount_amount1
   , amount - coalesce(coupon,0) AS discount_amount2
-from "064_purchase_log_with_coupon"
+from "064_purchase_log_with_coupon";
